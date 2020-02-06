c5250f08cb61922c627ea09de3aec51e42b18e68
Michal Piatkowski <piatkowski2002@gmail.com>
2020-02-06 07:33:37 +0100

    Changed some colors

---
+++ Source/Miniräknare/ColorObject.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+
+namespace Miniräknare
+{
+    public class ColorObject : INotifyPropertyChanged
+    {
+        private Color _color;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Color Color
+        {
+            get => _color;
+            set
+            {
+                if (_color != value)
+                {
+                    _color = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Color)));
+
+                    Brush.Color = value;
+                }
+            }
+        }
+
+        public SolidColorBrush Brush { get; }
+
+        public ColorObject(Color color)
+        {
+            _color = color;
+            Brush = new SolidColorBrush(_color);
+        }
+
+        public ColorObject() : this(Colors.White)
+        {
+        }
+    }
+}
